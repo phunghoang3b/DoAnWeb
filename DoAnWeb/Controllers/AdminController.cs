d79e0ebb62370de3f9857583e0b432e8a0da08d5
phunghoang3b <nguyenhoangkhai492000@gmail.com>
2021-07-04 23:47:30 +0700

    #1 nua lab 8

---
+++ DoAnWeb/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using DoAnWeb.Models;
+using System.Linq;
+using System.Web.Mvc;
+namespace DoAnWeb.Controllers
+{
+    public class AdminController : Controller
+    {
+        dbQLBanGameDataContext db = new dbQLBanGameDataContext();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            //Gán các giá trị người dùng nhập liệu cho các biến
+            var tendn = collection["username"];
+            var matkhau = collection["password"];
+            if (string.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (string.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                //Gán giá trị cho dối tượng được tạo mới (ad)
+                tblTaiKhoan ad = db.tblTaiKhoans.SingleOrDefault(n => n.Username == tendn && n.Password == matkhau);
+                if (ad != null)
+                {
+                    //ViewBag.Thongbao = "Chúc mừng bạn đăng nhập thành công";
+                    Session["Taikhoanadmin"] = ad;
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+            }
+            return View();
+        }
+    }
+}
