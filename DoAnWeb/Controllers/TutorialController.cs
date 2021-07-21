ceefb4951177e6c561ce914382932db1d1c1f6fd
phunghoang3b <nguyenhoangkhai492000@gmail.com>
2021-07-21 14:56:29 +0700

    #lam xong them 1 huong dan

--- DoAnWeb/Controllers/TutorialController.cs
+++ DoAnWeb/Controllers/TutorialController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace DoAnWeb.Controllers
 {
@@ -10,6 +6,10 @@ namespace DoAnWeb.Controllers
     {
         // GET: Tutorial
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Tuto1()
         {
             return View();
         }
